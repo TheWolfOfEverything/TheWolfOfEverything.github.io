088053fff9f494e23983bff9b55ba5eb86e1285d
TheWolfOfEverything <zjalbe494@west-mec.org>
2026-04-02 08:36:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-function showMessage() {
-  alert("Welcome! You clicked the button.");
-}
-
-function login(event) {
-  event.preventDefault();
-
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-  const message = document.getElementById("loginMessage");
-
-  if (username === "admin" && password === "1234") {
-    message.style.color = "lightgreen";
-    message.textContent = "Login successful!";
-  } else {
-    message.style.color = "red";
-    message.textContent = "Invalid username or password.";
-  }
+for (let i = 0; i < 5; i++) {
+  console.log(i); // Outputs 0, 1, 2, 3, 4
 }
