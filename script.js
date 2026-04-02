1fbea5cc8610ffd8a1bbf073ed749ddfeff5322c
TheWolfOfEverything <zjalbe494@west-mec.org>
2026-04-02 08:49:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 for (let i = 0; i < 500000000000; i++) {
-  console.log(i); // Outputs 0, 1, 2, 3, 4
+  alert("yoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
 }
