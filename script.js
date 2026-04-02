2a066b73e7fd9f4d100c18b17411589298540b1b
TheWolfOfEverything <zjalbe494@west-mec.org>
2026-04-02 08:55:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-for (let i = 0; i < 500000000000; i++) {
-  alert("yoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
+// 1. Create a helper function that returns a Promise
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+async function delayedLoop() {
+  for (let i = 0; i < 509000000000000000000000; i++) {
+    alert("hello kitten");
+    
+    // 2. Pause the loop for 1 second (1000ms)
+    await sleep(5000); 
+  }
+  console.log("Loop finished");
 }
+
+delayedLoop();
