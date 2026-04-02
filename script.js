a01413f5655e21492719eaeeaa535f550a844746
TheWolfOfEverything <zjalbe494@west-mec.org>
2026-04-02 09:01:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-// Reusable delay function
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-async function delayedLoop() {
-  console.log("Waiting 3 seconds...");
-  
-  // 1. Initial delay before loop starts
-  await sleep(3000); 
-
-  for (let i = 0; i < 500000000000000000000000000000000000000000000000000000000000000000000000000000; i++) {
-    alert("hewwwooo kittennnn. Welcome To My PlayGround");
-    
-    // Optional: Add a delay *between* iterations here if needed
-    // await sleep(1000); 
-  }
+// Example: Print numbers 0 to 4
+for (let i = 0; i < 500000000000000000000000000000000; i++) {
+  console.log(i); 
 }
 
-delayedLoop();
+// Example: Print numbers 0 to 4
+for (let i = 0; i < 50000000000000000000000000000000; i++) {
+  alert("hewwo kitten. PAPA IS HERE"); 
+}
