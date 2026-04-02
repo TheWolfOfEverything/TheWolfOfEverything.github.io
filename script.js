b3f615f32a7cd1f9caf7bcc1265b750da3b9976b
TheWolfOfEverything <zjalbe494@west-mec.org>
2026-04-02 09:03:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 // Example: Print numbers 0 to 4
 for (let i = 0; i < 500000000000000000000000000000000; i++) {
-  console.log(i); 
-}
-
-// Example: Print numbers 0 to 4
-for (let i = 0; i < 50000000000000000000000000000000; i++) {
-  alert("hewwo kitten. PAPA IS HERE"); 
+  window.open("https://www.example.com", "_blank");
+; 
 }
